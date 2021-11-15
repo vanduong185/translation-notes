75e8e095832a3d1d7183547f0ca09cf7ece9b01b
vanduong_nguyen <kysuhai@teneocto.io>
2021-11-15 11:56:16 +0700

    fix issue #11

--- renderer/webview-preload.js
+++ renderer/webview-preload.js
@@ -1,7 +1,7 @@
 const { ipcRenderer } = require('electron');
 
 ipcRenderer.on('get-translation', () => {
-  const source = window.document.getElementById('source').value;
-  const meaning = document.querySelectorAll('span.translation')[0].firstChild.textContent;
+  const source = document.querySelector('.vJwDU').textContent;
+  const meaning = document.querySelector('span.ChMk0b').firstChild.textContent;
   ipcRenderer.sendToHost('return-translation', {source, meaning});
 });
